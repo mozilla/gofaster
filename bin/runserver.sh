0d8b498d50503ff6ea9061022782c26fb58a5940
Cameron Dawson <cdawson@mozilla.com>
2013-01-23 11:01:48 -0800

    fixed error with processes not getting killed all the way and some gitignore updates.

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -7,7 +7,7 @@ cd $SERVER_DIR && ../../../bin/python server.py &
 PID=$!
 echo "Server running with PID $PID"
 
-trap "echo 'Killing server...'; kill -9 $PID" INT TERM EXIT
+trap "echo 'Killing server...'; pkill -P $PID" INT TERM EXIT
 while true; do
     sleep 60
 done
